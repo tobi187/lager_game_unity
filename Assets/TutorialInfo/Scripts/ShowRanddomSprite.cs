b1eb38edee60a5fcf99c1c1607110cccfd11dd1b
tobi187 <fischerstobi1998@gmail.com>
2022-06-06 19:10:32 +0200

    rendered images in carton

---
+++ Assets/TutorialInfo/Scripts/ShowRanddomSprite.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowRanddomSprite : MonoBehaviour
+{
+    public Sprite toyImage;
+    public Sprite drinkImage;
+    public Sprite sockImage;
+    private SpriteRenderer _currentComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var randomNum = Random.Range(0, 4);
+        _currentComponent = GetComponent<SpriteRenderer>();
+        var currSprite = new Sprite[] { toyImage, drinkImage, sockImage }[randomNum];
+        
+        _currentComponent.sprite = currSprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
