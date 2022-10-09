de6e08805ef951ee1ab633df9b8492ec8dda1c8c
tobi187 <fischerstobi1998@gmail.com>
2022-10-10 00:31:33 +0200

    doenst work

---
+++ Assets/TutorialInfo/Scripts/DragTarget.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragTarget : MonoBehaviour
+{
+    public Draggable item;
+    public int itemAcceptID;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
